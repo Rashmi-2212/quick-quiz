cd3d42bef3e5a30ba3d0bf38a291b6cd6130fa26
rashmi2212 <rrashmiguptaa@gmail.com>
2020-09-12 14:27:16 +0530

    "Adding new files"

---
+++ src/test/java/com/lockdown/learning/quickquiz/QuickQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lockdown.learning.quickquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
